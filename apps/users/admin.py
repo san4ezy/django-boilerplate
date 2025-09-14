889c1178675fcc1683a2ed927ec3e1641754e3f8
alexander <san4ezy@gmail.com>
2025-09-14 19:00:16 +0300

    V0.10

--- apps/users/admin.py
+++ apps/users/admin.py
@@ -1,8 +1,17 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
 from apps.users.models import User
 
 
 @admin.register(User)
-class UserAdmin(admin.ModelAdmin):
-    pass
+class UserAdmin(BaseUserAdmin):
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": ("phone_number", "password1", "password2"),
+            },
+        ),
+    )
